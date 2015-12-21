a4e6e908b429ee258e01d34d1661d84da5d7e737
Dennis Prangle <dennis.prangle@gmail.com>
2015-12-21 14:54:48 +0000

    Add an example for ABC

--- R/abc.R
+++ R/abc.R
@@ -104,6 +104,25 @@ rejABC = function(stats, obs, subset=1:ncol(stats), dist="weightedEuclidean", na
 #' @param nCores Number of parallel cores to use.
 #'
 #' @return A data frame containing parameters and summaries. If \code{output=="accepted"} then only the accepted simulations are returned. Otherwise all simulations are returns and an \code{output=="accepted"} column shows if they were accepted.
+#'
+#' @examples
+#'
+#' library(ismev)
+#' data(rain)
+#' doSim = function(pars) {
+#'     rainfallABC::simulation_Bartlett_Lewis(pars[1], pars[2], pars[3], 17531, 1) ##TO DO: dehardcode dur=17531
+#' }
+#' rprior = function() {
+#'     c(lambda = runif(1, 0, 2),
+#'       mu_x = runif(1, 0, 50),
+#'       eta = runif(1, 0, 10))
+#' }
+#' abcout = ABC(200, rprior, doSim, list(mean_overall, prop_dry, cor_overall, max_rain), rain)
+#' par(mfrow=c(1,3))
+#' for (i in 1:3) {
+#'     pname = names(abcout)[i]
+#'     hist(abcout[,i], main=pname, xlab=pname, freq=FALSE)
+#' }
 #' 
 #' @export
 #' @importFrom magrittr "%>%"
