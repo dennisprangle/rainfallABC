e2c6669824ff2124c119636edbbf6eefc2b68e96
Dennis Prangle <dennis.prangle@gmail.com>
2015-12-21 16:05:45 +0000

    Document summary statistic functions

--- R/sumstats.R
+++ R/sumstats.R
@@ -1,21 +1,57 @@
+#' Overall mean rainfall
+#'
+#' @param x Numeric vector of rainfall totals
+#'
+#' @return Mean rainfall
+#' 
 #' @export
 mean_overall = function(x) c(mean = mean(x))
 
+#' Proportion of dry days
+#'
+#' @param x Numeric vector of rainfall totals
+#' 
+#' @return Proportion of days with zero rainfall
 #' @export
 prop_dry = function(x) c(prop_dry = mean(x==0))
 
+#' Overall rainfall variance
+#'
+#' @param x Numeric vector of rainfall totals
+#' 
+#' @return Rainfall variance
 #' @export
 var_overall = function(x) c(var = var(x))
 
+#' Rainfall autocovariance
+#'
+#' @param x Numeric vector of rainfall totals
+#' 
+#' @return Lag 1 autocovariance
 #' @export
 cov_overall = function(x) c(cov = cov(x[-length(x)], x[-1]))
 
+#' Rainfall autocorrelation
+#'
+#' @param x Numeric vector of rainfall totals
+#' 
+#' @return Lag 1 autocorrelation
 #' @export
 cor_overall = function(x) c(cor = cor(x[-length(x)], x[-1]))
 
+#' Maximum rainfall
+#'
+#' @param x Numeric vector of rainfall totals
+#' 
+#' @return Maximum rainfall
 #' @export
 max_rain = function(x) c(max = max(x))
 
+#' Mean rainfall on wet days
+#'
+#' @param x Numeric vector of rainfall totals
+#' 
+#' @return Mean of rainfall on days with non-zero rain
 #' @export
 mean_wet = function(x) {
     wetcount = sum(x>0)
@@ -27,6 +63,12 @@ mean_wet = function(x) {
     c(mean_wet = mw)
 }
 
+#' Summaries related to streaks of consecutive rainy days
+#' 
+#' @param x Numeric vector of rainfall totals
+#'
+#' @return A vector of \code{mean_dur}, mean streak duration; \code{sd_dur}, standard deviation of streak durations; \code{mean_start_gap}, mean times between start times.
+#' Where not meaningfully defined the default values of these are 0, 0 and \code{length(x) + 1}.
 #' @export
 streaks = function(x) {    
     streak_durs = c() ##Durations of streaks (of all wet days)
