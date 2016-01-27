17160d437440dfbe5b075e16c49e2d87edfebebb
Dennis Prangle <dennis.prangle@gmail.com>
2016-01-27 16:41:48 +0000

    Update tests so summary functions can take extra args

--- tests/testthat/test-ABC.R
+++ tests/testthat/test-ABC.R
@@ -8,23 +8,21 @@ rdata = function(pars) {
     rnorm(100, pars[1], pars[2])
 }
 
-sum_functions = list(function(x) c(mean=mean(x)), function(x) c(sd=sd(x)))
+sum_functions = list(function(x, ...) c(mean=mean(x)), function(x, ...) c(sd=sd(x)))
 
 xobs = rnorm(100, 2, 2)
 
 ##simABC
-sims = rainfallABC:::simABC(1000, rprior, rdata, sum_functions)
-
-test_that("simABC outputs matrix of correct dimension", {
-         expect_true(is.matrix(sims))
-         expect_equal(dim(sims), c(1000, 4))
-     })
+test_that("simABC run and outputs matrix of correct dimension", {
+    sims = rainfallABC:::simABC(1000, rprior, rdata, sum_functions)
+    expect_true(is.matrix(sims))
+    expect_equal(dim(sims), c(1000, 4))
+})
 
 ##rejABC_1stat
-abcout1 = rainfallABC:::rejABC_1stat(sims[,"mean"], xobs[1], nacc=100)
-abcout2 = rainfallABC:::rejABC_1stat(sims[,"mean"], xobs[1], dist="rank", nacc=100)
-
-test_that("rejABC_1stat outputs vector", {
+test_that("rejABC_1stat runs and outputs vector", {
+    abcout1 = rainfallABC:::rejABC_1stat(sims[,"mean"], xobs[1], nacc=100)
+    abcout2 = rainfallABC:::rejABC_1stat(sims[,"mean"], xobs[1], dist="rank", nacc=100)
     expect_true(is.vector(abcout1))
     expect_true(length(abcout1) == 100)
     expect_true(is.vector(abcout2))
@@ -32,10 +30,9 @@ test_that("rejABC_1stat outputs vector", {
 })
 
 ##rejABC
-abcout3 = rainfallABC:::rejABC(sims[,3:4], xobs[3:4], nacc=100)
-abcout4 = rainfallABC:::rejABC(sims[,3:4], xobs[3:4], dist="rank", nacc=100)
-
-test_that("rejABC outputs vector", {
+test_that("rejABC runs and outputs vector", {
+    abcout3 = rainfallABC:::rejABC(sims[,3:4], xobs[3:4], nacc=100)
+    abcout4 = rainfallABC:::rejABC(sims[,3:4], xobs[3:4], dist="rank", nacc=100)
     expect_true(is.vector(abcout3))
     expect_true(length(abcout1) == 100)
     expect_true(is.vector(abcout4))
@@ -48,7 +45,7 @@ abcout6 = ABC(1000, rprior, rdata, sum_functions, xobs, nacc=100, output="all")
 abcout7 = ABC(1000, rprior, rdata, sum_functions, xobs, dist="rank", nacc=100, output="accepted")
 abcout8 = ABC(1000, rprior, rdata, sum_functions, xobs, dist="rank", nacc=100, output="all")
 
-test_that("ABC output correct", {
+test_that("ABC runs and gives correct output", {
     expect_true(is.data.frame(abcout5))
     expect_true(is.data.frame(abcout6))
     expect_true(is.data.frame(abcout7))
